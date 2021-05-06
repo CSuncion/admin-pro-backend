9804891b657c98e3a034c8dee30ff6b8a7346e41
Cristhian Suncion <cristhian.suncion@intelica.com>
2021-05-06 17:26:47 -0500

    Fin seccion 12

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
     const { email, password } = req.body;
@@ -43,10 +44,54 @@ const login = async (req, res = response) => {
         res.status(500).json({
             ok: false,
             msg: 'Comuniquese con el administrador'
-        })
+        });
     }
 }
 
+
+const googleSignIn = async (req, res = response) => {
+    const googleToken = req.body.token;
+
+    try {
+        const { name, email, picture } = await googleVerify(googleToken);
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+        if (!usuarioDB) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            });
+        } else {
+            // existe usuario
+            usuario = usuarioDB;
+            usuario.google = true;
+            // usuario.password = '@@@';
+        }
+
+        // Guardar en DB
+        await usuario.save();
+
+        // Generar el Token - JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok: true,
+            token
+        })
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: 'token no es correcto'
+        })
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- index.js
+++ index.js
@@ -16,13 +16,16 @@ app.use(express.json());
 // Conexion DB
 dbConnection();
 
+// Directorio publico
+app.use(express.static('public'));
+
 // Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/hospitales', require('./routes/hospitales'));
 app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
-app.use('/api/login',require('./routes/auth'));
-app.use('/api/upload',require('./routes/uploads'));
+app.use('/api/login', require('./routes/auth'));
+app.use('/api/upload', require('./routes/uploads'));
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT);
